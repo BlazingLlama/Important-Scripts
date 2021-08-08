d9bad59c91b3b37f31aa9b68dbbca1a3033404b6
BlazingLlama <59803332+Lukki163@users.noreply.github.com>
2021-08-08 20:43:13 +0300

    Update DamageAmount.cs

--- FPS STUFF/DamageAmount.cs
+++ FPS STUFF/DamageAmount.cs
@@ -4,5 +4,10 @@ using UnityEngine;
 
 public class DamageAmount : MonoBehaviour
 {
+
+//MADE BY BLAZINGLLAMA
+//BASIC BULLET DAMAGE SCRIPT FOR PLAYERDIESCRIPT.CS
+//PUT ON BULLET/PRIJECTILE PREFAB
+//NEED HELP SETTING UP? MY DISCORD: BlazingLlama#2782
     public float BulletDamage;
 }
