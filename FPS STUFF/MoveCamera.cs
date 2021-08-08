c5d48e2d8032fa54a16428431490f64de3a8bc7b
BlazingLlama <59803332+Lukki163@users.noreply.github.com>
2021-08-08 20:38:53 +0300

    Update MoveCamera.cs

--- FPS STUFF/MoveCamera.cs
+++ FPS STUFF/MoveCamera.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//MADE BY BLAZING LLAMA
+//SIMPLE SCRIPT FOR RBMOVEMENT.CS THAT HELPS WITH CAMERA JITTERING
+//NEED HELP SETTING UP? MY DISCORD: BlazingLlama#2782
+
 public class MoveCamera : MonoBehaviour
 {
     [SerializeField] Transform CameraPosition;
